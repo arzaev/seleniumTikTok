156f8be39171432c53fe39f5ade7a58bc8e746fc
Ilya <arzaevilya@gmail.com>
2021-09-10 20:51:28 +0700

    filter parsing videos

--- mouse_clicker.py
+++ mouse_clicker.py
@@ -1,18 +1,24 @@
 from pynput.mouse import Button, Controller
+from pynput.keyboard import Controller as keycontr
+from pynput.keyboard import Key
+import undetected_chromedriver as uc
 import time
 from selenium import webdriver
+
 import pickle
 
 
 class SenTiktok:
 
     def auth(self):
-        options = webdriver.ChromeOptions()
+        options = uc.ChromeOptions()
         options.add_argument('--disable-blink-features=AutomationControlled')
-        self.driver = webdriver.Chrome(options=options)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        self.driver = uc.Chrome(options=options)
         self.driver.get('https://www.tiktok.com/@charlidamelio?')
 
-    def start(self, parsing):
+    def follow(self, parsing):
         with open(parsing, 'r') as f:
             parsing_list = f.readlines()
         count = 0
@@ -30,6 +36,53 @@ class SenTiktok:
                 print('error: ' + user)
             count += 1
 
+    def like(self, parsing):
+        with open(parsing, 'r') as f:
+            parsing_list = f.readlines()
+        count = 0
+        for video in parsing_list:
+            try:
+                self.driver.get(video)
+                time.sleep(5)
+                # self.driver.find_element_by_tag_name("body").send_keys("l")
+                keyboard = keycontr()
+                keyboard.press('l')
+                keyboard.release('l')
+                time.sleep(5)
+                print('video {}: {}'.format(str(video), str(count)))
+            except Exception as error:
+                print(error)
+                print('error: ' + video)
+            count += 1
+
+    def comment(self, parsing):
+        with open(parsing, 'r') as f:
+            parsing_list = f.readlines()
+        count = 0
+        for video in parsing_list:
+            try:
+                self.driver.get(video)
+                time.sleep(5)
+                button_comments = self.driver.find_element_by_xpath('//*[@id="main"]/div[2]/div[2]/div/div/main/div/div[1]/span[1]/div/div[1]/div[5]/div[2]/div[2]/div')
+                button_comments.click()
+                time.sleep(5)
+
+                mouse = Controller()
+                mouse.position = (1500, 1000)
+                mouse.click(Button.left, 1)
+                time.sleep(2)
+                keyboard = keycontr()
+                keyboard.type("hello")
+                keyboard.press(Key.enter)
+                keyboard.release(Key.enter)
+                time.sleep(2)
+
+                print('video {}: {}'.format(str(video), str(count)))
+            except Exception as error:
+                print(error)
+                print('error: ' + video)
+            count += 1
+
     def save_cookies(self, name_cookies):
         pickle.dump(self.driver.get_cookies(), open("{}.pkl".format(name_cookies), "wb"))
 
@@ -37,6 +90,9 @@ class SenTiktok:
         cookies = pickle.load(open("{}.pkl".format(name_cookies), "rb"))
         for cookie in cookies:
             print(cookie)
+            if 'sameSite' in cookie:
+                if cookie['sameSite'] == 'None':
+                    cookie['sameSite'] = 'Strict'
             self.driver.add_cookie(cookie)
 
 
@@ -48,9 +104,15 @@ if __name__ == "__main__":
         print(command)
         if 'auth' in command:
             tiktok.auth()
-        if 'start' in command:
+        if 'follow' in command:
             parsing_list = command.split('-p ')[1]
-            tiktok.start(parsing_list)
+            tiktok.follow(parsing_list)
+        if 'like' in command:
+            parsing_list = command.split('-p ')[1]
+            tiktok.like(parsing_list)
+        if 'comment' in command:
+            parsing_list = command.split('-p ')[1]
+            tiktok.comment(parsing_list)
         if 'save cookies' in command:
             name_cookies = command.split('-n ')[1]
             tiktok.save_cookies(name_cookies)
--- parsing_videos.py
+++ parsing_videos.py
@@ -7,34 +7,14 @@ proxies = {
   'https': 'http://192.168.0.125:8080',
 }
 
-class Login:
-    def __init__(self, proxy=True,
-                 item_proxy='192.168.0.125:8080',
-                 type_proxy='https',
-                 user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
-                 verify=False, timeout=10):
+
+class ParsingVideos:
+    def __init__(self, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'):
         self.user_agent = user_agent
         self.session = requests.Session()
-        self.item_proxy = item_proxy
-        self.type_proxy = type_proxy
-        if type_proxy == 'socks5':
-            self.proxies = {
-                'http': 'socks5://{}'.format(item_proxy),
-                'https': 'socks5://{}'.format(item_proxy)
-            }
-        else:
-            if '@' in item_proxy:
-                self.proxies = {'http': 'http://{}'.format(item_proxy),
-                                'https': 'https://{}'.format(item_proxy)}
-            else:
-                self.proxies = {'http': item_proxy, 'https': item_proxy}
-        self.verify = verify
-        self.timeout = timeout
-        self.list = []
-     #   self.proxies = None
 
-    def start(self, login):
-        url = 'https://www.tiktok.com/@{}?'.format(login)
+    def start(self, username):
+        url = 'https://www.tiktok.com/@{}'.format(username)
         headers = {'Host': 'www.tiktok.com',
                     'User-Agent': self.user_agent,
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -48,19 +28,40 @@ class Login:
                     'Te': 'trailers',
                     }
         r = self.session.get(url, headers=headers, proxies=proxies, verify=False)
-        if 'blogspot' in r.text:
-            print('good: ' + login)
-            with open('blogspotaccs.txt', 'a') as f:
-                f.write(login + '\n')
+        if '"items":[{"id":"' in r.text:
+            id_video = r.text.split('"items":[{"id":"')[1].split('"')[0]
+            video_link = "https://www.tiktok.com/@{}/video/{}?is_copy_url=1&is_from_webapp=v1".format(username, id_video)
+
+            headers = {'Host': 'www.tiktok.com',
+                       'User-Agent': self.user_agent,
+                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                       'Accept-Language': 'en-US,en;q=0.5',
+                       'Accept-Encoding': 'gzip, deflate',
+                       'Upgrade-Insecure-Requests': '1',
+                       'Sec-Fetch-Dest': 'document',
+                       'Sec-Fetch-Mode': 'navigate',
+                       'Sec-Fetch-Site': 'none',
+                       'Sec-Fetch-User': '?1',
+                       'Te': 'trailers',
+                       }
+            r = self.session.get(video_link, headers=headers, proxies=proxies, verify=False)
+            stat = r.text.split('"stats":{')[1].split('"isActivityItem"')[0]
+            likes = stat.split('"diggCount":')[1].split(',')[0]
+            comments = stat.split('"commentCount":')[1].split(',')[0]
+            print(video_link)
+            print(stat)
+            if int(likes) < 50:
+                if int(comments) < 50:
+                with open('out_videos.txt', 'a') as f:
+                    f.write(video_link + '\n')
+
 
 with open('input.txt', 'r') as f:
-    logins = f.read().split('\n')
+    logins = f.readlines()
 
 
 for i in logins:
-    login = Login()
-    l = i.split(':')[1]
-    print(l)
-    login.start(l)
+    parsing_videos = ParsingVideos()
+    parsing_videos.start(i.strip())
 
 
